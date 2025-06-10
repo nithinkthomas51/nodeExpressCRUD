bbf6c23eab0936db23da7bd1c014423d5511d9e3
Nithin K Thomas <64864340+nithinkthomas51@users.noreply.github.com>
2025-06-10 15:36:11 +0000

    Setup express js and created a simple route

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+
+const app = express();
+const PORT = 5000;
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.send('Hi from server');
+});
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
